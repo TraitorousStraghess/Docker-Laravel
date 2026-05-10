1eec108669fb8b6c99faa5f8f70347a281d5b028
masaya <yoma5128@gmail.com>
2026-05-10 18:11:58 +0900

    Bladeの基本

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -10,3 +10,14 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/profile', function () {
+    $username = 'Taro';
+    $is_admin = false;
+    $skills = ['PHP', 'HTML', 'JS'];
+    return view('profile', [ // profile.blade.php にデータを渡す
+        'name' => $username,
+        'is_admin' => $is_admin,
+        'skills' => $skills
+    ]);
+});
