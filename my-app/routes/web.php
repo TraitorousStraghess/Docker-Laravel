3dbd52ea8c40ae5713dbd9b799c40ec008820210
masaya <yoma5128@gmail.com>
2026-05-10 17:06:11 +0900

    ルーターの書き方

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 静的なhtmlを返す
+Route::get('/about', function () {
+    return view('about');
+});
